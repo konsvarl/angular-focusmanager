37562527ff8030fd8cd1e08fbedb80c83efa7186
Rob Taylor <roboncode@gmail.com>
2015-03-19 12:36:20 -0600

    removed examples

--- examples/js/example.js
+++
@@ -1,9 +0,0 @@
-/* global angular */
-var app = angular.module('app', ['fm']);
-app.directive('main', function ($log) {
-    return {
-        link: function (scope) {
-            scope.log = $log.log;
-        }
-    };
-});
